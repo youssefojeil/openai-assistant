7b090aa4129839bd6910df6512ab431e5647fa67
Youssef <youssef.ojeil@hotmail.com>
2024-03-07 13:42:53 -0500

    create readable stream to and from openai, log readable stream to browser console

--- app/api/message/route.js
+++ app/api/message/route.js
@@ -1,5 +1,6 @@
 import { MessageArraySchema } from '@/lib/validators/message';
 import { chatbotPrompt } from '@/helpers/constants/chatbot-prompt';
+import { OpenAIStream } from '@/lib/openai-stream';
 
 export async function POST(req) {
   console.log('endpoint works');
@@ -17,4 +18,20 @@ export async function POST(req) {
     role: 'system',
     content: chatbotPrompt,
   });
+
+  const payload = {
+    model: 'gpt-3.5-turbo',
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+
+  return new Response(stream);
 }
--- components/chat-input.jsx
+++ components/chat-input.jsx
@@ -16,14 +16,26 @@ const ChatInput = ({ className, ...props }) => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ messages: 'hello' }),
+        body: JSON.stringify({ messages: [message] }),
       });
 
       return response.body;
     },
 
-    onSuccess: () => {
-      console.log('Success');
+    onSuccess: async (stream) => {
+      if (!stream) throw new Error('No Stream found');
+
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+
+      let done = false;
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        console.log(chunkValue);
+      }
     },
   });
 
